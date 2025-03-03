5c653bd87b13af4920b4b4e85649309de8be26c5
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-03-03 11:23:47 -0800

    Basic working local copy of TanStack Form and Cloudflare Workers AI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,133 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useForm } from "@tanstack/react-form";
+import type { AnyFieldApi } from "@tanstack/react-form";
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <em>{field.state.meta.errors.join(", ")}</em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      feedback: "",
+    },
+    onSubmit: async ({ value }) => {
+      let baseUrl = "";
+      if (process.env.NODE_ENV === "development") {
+        baseUrl = "http://127.0.0.1:8787";
+      }
+      const response = await fetch(`${baseUrl}/submit`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value),
+      });
+      const data = await response.json();
+      console.log(data);
+    },
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tanstack Form + Cloudflare Workers AI</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+        style={{ display: 'flex', flexDirection: 'column', gap: '1rem', maxWidth: '400px', margin: 'auto' }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          {/* A type-safe field component*/}
+          <form.Field
+            name="name"
+            validators={{
+              onChange: ({ value }) =>
+                !value
+                  ? "Name is required"
+                  : value.length < 3
+                  ? "Your name must be least 3 characters"
+                  : undefined,
+              onChangeAsyncDebounceMs: 500,
+              onChangeAsync: async ({ value }) => {
+                await new Promise((resolve) => setTimeout(resolve, 500));
+                return (
+                  value.includes("error") && 'No "error" allowed in first name'
+                );
+              },
+            }}
+            children={(field) => {
+              // Avoid hasty abstractions. Render props are great!
+              return (
+                <>
+                  <label htmlFor={field.name}>First Name:</label>
+                  <input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  <FieldInfo field={field} />
+                </>
+              );
+            }}
+          />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+          <form.Field
+            name="feedback"
+            validators={{
+              onChange: ({ value }) =>
+                !value
+                  ? "Feedback is a required field"
+                  : value.length < 10
+                  ? "Your feedback must be least 10 characters"
+                  : undefined,
+              onChangeAsyncDebounceMs: 500,
+              onChangeAsync: async ({ value }) => {
+                await new Promise((resolve) => setTimeout(resolve, 500));
+                return (
+                  value.includes("error") && 'No "error" allowed in feedback'
+                );
+              },
+            }}
+            children={(field) => (
+              <>
+                <label htmlFor={field.name}>Feedback:</label>
+                <textarea
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                <FieldInfo field={field} />
+              </>
+            )}
+          />
+        </div>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]: [boolean, boolean]) => (
+            <button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "..." : "Submit"}
+            </button>
+          )}
+        />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -11,8 +11,83 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface SentimentResult {
+	label: string;
+	score: number;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const corsHeaders = getCorsHeaders();
+		if (handleOptionsRequest(request, corsHeaders)) {
+			return new Response('OK', {
+				headers: corsHeaders,
+			});
+		}
+
+		const url = new URL(request.url);
+
+		if (request.method === 'POST' && url.pathname === '/submit') {
+			const data = await request.json();
+			const { name, feedback } = data as { name: string; feedback: string };
+			console.log(`Name: ${name}, Feedback: ${feedback}`); // Handle the data as needed
+
+			const sentiment = await getSentimentScoreWithWorkersAI(env, feedback);
+			console.log(sentiment);
+			await env.KV.put(name, JSON.stringify(sentiment));
+
+			return new Response(JSON.stringify({ message: 'Data received', sentiment }), {
+				headers: corsHeaders,
+			});
+		}
+
+		// if (request.method === 'GET' && url.pathname === '/recent') {
+		// 	const keys = await env.KV.list();
+		// 	const values = await Promise.all(
+		// 		keys.keys.map(async (key) => {
+		// 			const value = await env.KV.get(key.name);
+		// 			return { key: key.name, value: value ? JSON.parse(value) : null };
+		// 		})
+		// 	);
+		// 	console.log("KV: ", values);
+		// 	return new Response(JSON.stringify(values), {
+		// 		headers: corsHeaders,
+		// 	});
+		// }
+
+		return new Response(JSON.stringify('Hello World'), {
+			headers: corsHeaders,
+		});
 	},
 } satisfies ExportedHandler<Env>;
+
+async function getSentimentScoreWithWorkersAI(env: Env, feedback: string) {
+	const response = await env.AI.run('@cf/huggingface/distilbert-sst-2-int8', {
+		text: feedback,
+	});
+
+	const sentimentResult = response.reduce((prev, current) => {
+		return (prev as SentimentResult).score > (current as SentimentResult).score ? prev : current;
+	});
+
+	const sentiment = {
+		label: sentimentResult.label,
+		score: sentimentResult.score,
+	};
+	return sentiment;
+}
+
+function getCorsHeaders() {
+	return {
+		'Access-Control-Allow-Headers': '*',
+		'Access-Control-Allow-Methods': 'POST',
+		'Access-Control-Allow-Origin': '*',
+	};
+}
+
+function handleOptionsRequest(request: Request, corsHeaders: HeadersInit): boolean {
+	if (request.method === 'OPTIONS') {
+		return true;
+	}
+	return false;
+}
